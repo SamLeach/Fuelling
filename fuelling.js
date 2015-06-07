8fecd8f1ea176d89369cfbbd87410e1d7a6654f0
Sam Leach <leachsam@gmail.com>
2015-06-07 15:28:12 +0200

    New home (rough)
    Rough draft of home.

--- fuelling.js
+++ fuelling.js
@@ -3,11 +3,16 @@ $(document).ready(function() {
 		sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff']
 	});
 
+	$(".dropdown-menu li a").click(function(){
+	    $(".caret:first-child").text($(this).text());
+	     $(".caret:first-child").val($(this).text());
+  	});
+
 	var fixSection0Position = function(){
 		$('#section0')
 		.children('div')
 		.first()
-		.css('vertical-align','baseline');
+		.css('vertical-align','baseline');	
 
 		$('#section7')
 		.children('div')
